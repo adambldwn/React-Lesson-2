1a904ef33667386fe49bf38ce00750496e97cadb
efe <adambldwn110@gmail.com>
2020-11-29 18:58:59 +0300

    navbar

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -4,11 +4,11 @@ import firebase from "../firebase/firebase.utils";
 export const FirebaseAuthContext = createContext();
 
 function AuthContextProvider(props) {
-  const [isAuthenticated, setAuthonticated] = useState(false);
   const [currentUser, setCurrentUser] = useState();
 
   useEffect(() => {
     firebase.firebaseAuth.onAuthStateChanged((user) => {
+      console.log("user", user);
       setCurrentUser(user);
     });
   }, []);
